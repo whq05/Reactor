2b5a3494c28cbfd099a7494f57a4801f5e57038c
ubuntu_whq05 <hqw050829@gmail.com>
2025-03-24 10:32:37 +0800

    把socket的库函数封装成Socket类

--- InetAddress.h
+++ InetAddress.h
@@ -8,13 +8,15 @@
 class InetAddress
 {
 private:
-    sockaddr_in addr_;        // 表示地址协议的结构体
+    sockaddr_in addr_; // 表示地址协议的结构体
 public:
-    InetAddress(const std::string &ip,uint16_t port);      // 如果是监听的fd，用这个构造函数
-    InetAddress(const sockaddr_in addr);                       // 如果是客户端连上来的fd，用这个构造函数
+    InetAddress();
+    InetAddress(const std::string &ip, uint16_t port); // 如果是监听的fd，用这个构造函数
+    InetAddress(const sockaddr_in addr);               // 如果是客户端连上来的fd，用这个构造函数
     ~InetAddress();
 
-    const char *ip() const;                // 返回字符串表示的地址，例如：192.168.206.132
-    uint16_t    port() const;              // 返回整数表示的端口，例如：80、8080
-    const sockaddr *addr() const;   // 返回addr_成员的地址，转换成了sockaddr
+    const char *ip() const;               // 返回字符串表示的地址，例如：192.168.206.132
+    uint16_t port() const;                // 返回整数表示的端口，例如：80、8080
+    const sockaddr *addr() const;         // 返回addr_成员的地址，转换成了sockaddr
+    void setaddr(sockaddr_in clientaddr); // 设置addr_成员的值
 };
--- tcpepoll.cpp
+++ tcpepoll.cpp
@@ -4,22 +4,24 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/socket.h>
-#include <sys/types.h>          
+#include <sys/types.h>
 #include <arpa/inet.h>
 #include <sys/fcntl.h>
 #include <sys/epoll.h>
-#include <netinet/tcp.h>      // TCP_NODELAY需要包含这个头文件
+#include <netinet/tcp.h> // TCP_NODELAY需要包含这个头文件
 #include "InetAddress.h"
+#include "Socket.h"
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc != 3) 
-    { 
-        printf("usage: ./tcpepoll ip port\n"); 
-        printf("example: ./tcpepoll 192.168.206.132 5010\n\n"); 
-        return -1; 
+    if (argc != 3)
+    {
+        printf("usage: ./tcpepoll ip port\n");
+        printf("example: ./tcpepoll 192.168.206.132 5010\n\n");
+        return -1;
     }
 
+    /*
     // 创建服务端用于监听的listenfd
     int listenfd = socket(AF_INET,SOCK_STREAM|SOCK_NONBLOCK,IPPROTO_TCP);
     if (listenfd < 0)
@@ -27,7 +29,7 @@ int main(int argc,char *argv[])
         perror("socket() failed"); return -1;
     }
 
-    int opt = 1; 
+    int opt = 1;
     setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&opt,static_cast<socklen_t>(sizeof opt));    // 必须的
     setsockopt(listenfd,SOL_SOCKET,TCP_NODELAY   ,&opt,static_cast<socklen_t>(sizeof opt));    // 必须的
     setsockopt(listenfd,SOL_SOCKET,SO_REUSEPORT ,&opt,static_cast<socklen_t>(sizeof opt));    // 有用，但是，在Reactor中意义不大
@@ -48,99 +50,114 @@ int main(int argc,char *argv[])
     {
         perror("listen() failed"); close(listenfd); return -1;
     }
+    */
+    Socket servsock(createnonblocking());
+    InetAddress servaddr(argv[1], atoi(argv[2])); // 服务端的地址和协议。
+    servsock.setreuseaddr(true);
+    servsock.settcpnodelay(true);
+    servsock.setreuseport(true);
+    servsock.setkeepalive(true);
+    servsock.bind(servaddr);
+    servsock.listen();
 
-    int epollfd=epoll_create(1);        // 创建epoll句柄（红黑树）
+    int epollfd = epoll_create(1); // 创建epoll句柄（红黑树）
 
     // 为服务端的listenfd准备读事件
-    struct epoll_event ev;              // 声明事件的数据结构
-    ev.data.fd=listenfd;       // 指定事件的自定义数据，会随着epoll_wait()返回的事件一并返回
-    ev.events=EPOLLIN;      // 让epoll监视listenfd的读事件，采用水平触发
+    struct epoll_event ev;      // 声明事件的数据结构
+    ev.data.fd = servsock.fd(); // 指定事件的自定义数据，会随着epoll_wait()返回的事件一并返回
+    ev.events = EPOLLIN;        // 让epoll监视listenfd的读事件，采用水平触发
 
-    epoll_ctl(epollfd,EPOLL_CTL_ADD,listenfd,&ev);     // 把需要监视的listenfd和它的事件加入epollfd中
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, servsock.fd(), &ev); // 把需要监视的listenfd和它的事件加入epollfd中
 
-    struct epoll_event evs[10];      // 存放epoll_wait()返回事件的数组
+    struct epoll_event evs[10]; // 存放epoll_wait()返回事件的数组
 
-    while (true)        // 事件循环
+    while (true) // 事件循环
     {
-        int infds=epoll_wait(epollfd,evs,10,-1);       // 等待监视的fd有事件发生
+        int infds = epoll_wait(epollfd, evs, 10, -1); // 等待监视的fd有事件发生
 
         // 返回失败
         if (infds < 0)
         {
-            perror("epoll_wait() failed"); break;
+            perror("epoll_wait() failed");
+            break;
         }
 
         // 超时
         if (infds == 0)
         {
-            printf("epoll_wait() timeout.\n"); continue;
+            printf("epoll_wait() timeout.\n");
+            continue;
         }
 
         // 如果infds>0，表示有事件发生的fd的数量
-        for (int ii=0;ii<infds;ii++)       // 遍历epoll返回的数组evs
+        for (int ii = 0; ii < infds; ii++) // 遍历epoll返回的数组evs
         {
-            if (evs[ii].events & EPOLLRDHUP)                     // 对方已关闭，有些系统检测不到，可以使用EPOLLIN，recv()返回0
+            if (evs[ii].events & EPOLLRDHUP) // 对方已关闭，有些系统检测不到，可以使用EPOLLIN，recv()返回0
             {
-                printf("client(eventfd=%d) disconnected.\n",evs[ii].data.fd);
-                close(evs[ii].data.fd);            // 关闭客户端的fd
-            }                              
-            else if (evs[ii].events & (EPOLLIN|EPOLLPRI))   // 接收缓冲区中有数据可以读
+                printf("client(eventfd=%d) disconnected.\n", evs[ii].data.fd);
+                close(evs[ii].data.fd); // 关闭客户端的fd
+            }
+            else if (evs[ii].events & (EPOLLIN | EPOLLPRI)) // 接收缓冲区中有数据可以读
             {
-                if (evs[ii].data.fd==listenfd)   // 如果是listenfd有事件，表示有新的客户端连上来
+                if (evs[ii].data.fd == servsock.fd()) // 如果是listenfd有事件，表示有新的客户端连上来
                 {
+                    /*
                     sockaddr_in peeraddr;
                     socklen_t len = sizeof(peeraddr);
-                    int clientfd = accept4(listenfd,(sockaddr*)&peeraddr,&len,SOCK_NONBLOCK);
+                    int clientfd = accept4(listenfd, (sockaddr *)&peeraddr, &len, SOCK_NONBLOCK);
+                    */
+                    InetAddress clientaddr;             // 客户端的地址和协议。
+                    // 注意，clientsock只能new出来，不能在栈上，否则析构函数会关闭fd。
+                    // 还有，这里new出来的对象没有释放，这个问题以后再解决
+                    Socket *clientsock=new Socket(servsock.accept(clientaddr));
 
-                    InetAddress clientaddr(peeraddr);             // 客户端的地址和协议
-
-                    printf ("accept client(fd=%d,ip=%s,port=%d) ok.\n",clientfd,clientaddr.ip(),clientaddr.port());
+                    printf ("accept client(fd=%d,ip=%s,port=%d) ok.\n",clientsock->fd(),clientaddr.ip(),clientaddr.port());
 
                     // 为新客户端连接准备读事件，并添加到epoll中
-                    ev.data.fd=clientfd;
-                    ev.events=EPOLLIN|EPOLLET;           // 边缘触发
-                    epoll_ctl(epollfd,EPOLL_CTL_ADD,clientfd,&ev);
+                    ev.data.fd = clientsock->fd();
+                    ev.events = EPOLLIN | EPOLLET; // 边缘触发
+                    epoll_ctl(epollfd, EPOLL_CTL_ADD, clientsock->fd(), &ev);
                 }
-                else                                        // 如果是客户端连接的fd有事件
+                else // 如果是客户端连接的fd有事件
                 {
                     char buffer[1024];
-                    while (true)             // 由于使用非阻塞IO，一次读取buffer大小数据，直到全部的数据读取完毕
-                    {    
+                    while (true) // 由于使用非阻塞IO，一次读取buffer大小数据，直到全部的数据读取完毕
+                    {
                         bzero(&buffer, sizeof(buffer));
                         ssize_t nread = read(evs[ii].data.fd, buffer, sizeof(buffer));
-                        if (nread > 0)      // 成功的读取到了数据
+                        if (nread > 0) // 成功的读取到了数据
                         {
                             // 把接收到的报文内容原封不动的发回去
-                            printf("recv(eventfd=%d):%s\n",evs[ii].data.fd,buffer);
-                            send(evs[ii].data.fd,buffer,strlen(buffer),0);
-                        } 
+                            printf("recv(eventfd=%d):%s\n", evs[ii].data.fd, buffer);
+                            send(evs[ii].data.fd, buffer, strlen(buffer), 0);
+                        }
                         else if (nread == -1 && errno == EINTR) // 读取数据的时候被信号中断，继续读取
-                        {  
+                        {
                             continue;
-                        } 
+                        }
                         else if (nread == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))) // 全部的数据已读取完毕
                         {
                             break;
-                        } 
-                        else if (nread == 0)  // 客户端连接已断开
-                        {  
-                            printf("client(eventfd=%d) disconnected.\n",evs[ii].data.fd);
-                            close(evs[ii].data.fd);            // 关闭客户端的fd
+                        }
+                        else if (nread == 0) // 客户端连接已断开
+                        {
+                            printf("client(eventfd=%d) disconnected.\n", evs[ii].data.fd);
+                            close(evs[ii].data.fd); // 关闭客户端的fd
                             break;
                         }
                     }
                 }
             }
-            else if (evs[ii].events & EPOLLOUT)                  // 有数据需要写，暂时没有代码
+            else if (evs[ii].events & EPOLLOUT) // 有数据需要写，暂时没有代码
             {
             }
-            else                                                                   // 其它事件，都视为错误
+            else // 其它事件，都视为错误
             {
-                printf("3client(eventfd=%d) error.\n",evs[ii].data.fd);
-                close(evs[ii].data.fd);            // 关闭客户端的fd
+                printf("3client(eventfd=%d) error.\n", evs[ii].data.fd);
+                close(evs[ii].data.fd); // 关闭客户端的fd
             }
         }
     }
 
-  return 0;
+    return 0;
 }
